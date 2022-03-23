a351eb685848694bd3b0d07456bcc44f9d7b4de0
yryskul <kuttubaevayryskul@gmail.com>
2022-03-23 14:57:21 +0600

    added div>h1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Paragraph from "./componets/Paragraph/Paragaph";
 function App() {
   return(
     <div className="App">
     <h1>Hello world</h1>
+    <Paragraph size="16px"> My name is Ryskul</Paragraph>
+    <Paragraph size=".8rem">My name is Ryskul</Paragraph>
     </div>
+
   );
 }
 
