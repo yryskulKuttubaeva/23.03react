03070b3b7d4afba6ca86869307a2847df81bf75c
yryskul <kuttubaevayryskul@gmail.com>
2022-03-24 12:33:48 +0600

    add img

---
+++ src/componets/Image/Image.js
@@ -0,0 +1,16 @@
+import "./Image.css";
+
+function Image(props) {
+  const className = ["Image", props.align].join(" ");
+  const style = {
+    width: props.width,
+    height: props.height,
+  };
+
+  return <img
+    className={className}
+    style={style}
+    src={props.url} />;
+}
+
+export default Image;
