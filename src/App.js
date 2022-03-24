8422f7d1bd7471b21a9e9a3bed13252919301af2
yryskul <kuttubaevayryskul@gmail.com>
2022-03-24 12:32:42 +0600

    added component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Paragraph from "./componets/Paragraph/Paragaph";
-function App() {
-  return(
-    <div className="App">
-    <h1>Hello world</h1>
-    <Paragraph size="16px"> My name is Ryskul</Paragraph>
-    <Paragraph size=".8rem">My name is Ryskul</Paragraph>
-    </div>
+import Image from './components/Image/Image';
+import Paragraph from './components/Paragraph/Paragraph';
 
+import koalaImage from './assets/koala.jpg';
+import Alert from './components/Alert/Alert';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+
+      <Alert success>This is a sample alert</Alert>
+      <Alert danger>This is a sample alert</Alert>
+      <Image align="left" url={koalaImage} width="150px" height="100px" />
+      <Image align="right" url="https://upload.wikimedia.org/wikipedia/commons/4/49/Koala_climbing_tree.jpg" width="150px" height="100px" />
+      <Paragraph size="14px">This is sample paragraph</Paragraph>
+      <Paragraph color="red">This is small paragraph</Paragraph>
+    </div>
   );
 }
 
